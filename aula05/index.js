bb8923335705878423757cda64b4cec725774d9b
Antônio Souza <antonioaugusto1913@gmail.com>
2023-03-27 12:58:11 -0300

    aula 04 e 05 + ex limite de velocidade

---
+++ aula05/index.js
@@ -0,0 +1,13 @@
+var vel = 60.1
+var limite = 60
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}else if (vel >=55){
+    console.log(`O limite de velocidade da rodovia é de ${limite}KM/h`)
+    console.log('Por pouco não ganha uma multa! PRESTE ATENÇÃO!')
+}
+
+
+console.log('Diriga sempre usando o cinto de segurança')
