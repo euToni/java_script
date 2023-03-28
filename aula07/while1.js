05d2ca9677b36412b5e67316c9f7977506442f10
Antônio Souza <antonioaugusto1913@gmail.com>
2023-03-28 19:32:32 -0300

    while - aula 07

---
+++ aula07/while1.js
@@ -0,0 +1,18 @@
+// console.log('opa')
+// console.log('opa')
+// console.log('opa')
+// console.log('opa')
+// console.log('opa')
+// console.log('opa')
+
+// var c = 1
+// while (c <= 6){
+//     console.log(`Passo ${c}`)
+//     c++
+// } 
+
+var c = 1
+do{
+    console.log('jett')
+    c++
+} while(c <= 6)
