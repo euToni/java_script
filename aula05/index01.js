4495a56a3b67797ed1de82cedb38a127a6fc3f91
Antônio Souza <antonioaugusto1913@gmail.com>
2023-04-06 09:51:30 -0300

    recomeçando js

--- aula05/index01.js
+++
@@ -1,7 +0,0 @@
-var pais = 'brasil'
-console.log(`Se você mora no(a) ${pais}`)
-if (pais == 'Brasil' || pais =='brasil' || pais == 'Brazil' || pais == 'brazil'){
-    console.log('Você é brasileiro')
-} else {
-    console.log('Você é estrangeiro')
-}
--- aula06/index.js
+++
@@ -1,9 +0,0 @@
-var idade = 18
-console.log(`Você tem ${idade} anos`)
-if (idade >= 18) {
-    console.log('Voto obrigatório')
-} else if (idade >=16 || idade >= 65){
-    console.log('Voto opcional')
-} else{
-    console.log('Não vota')
-}
